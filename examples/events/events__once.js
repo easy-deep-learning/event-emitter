663166042bcfe77f82b5dc69def344394bd75ac7
alexbaumgertner <alex.baumgertner@gmail.com>
2016-08-06 00:05:01 +0300

    Update 2016-08-05T21:04:58.440Z

---
+++ examples/events/events__once.js
@@ -0,0 +1,12 @@
+const EventEmitter = require('events');
+const myEmitter = new EventEmitter();
+
+myEmitter.once('myEvent', (data) => {
+  console.log(`Произошло событие myEvent, сообщение: ${data.message}`);
+});
+
+myEmitter.emit('myEvent', {message: "Привет, Мир!"});
+
+// Не будет выведено
+myEmitter.emit('myEvent', {message: "Еще раз, Привет, Мир!"});
+
---
+++ examples/events/events__prepends.js
@@ -0,0 +1,13 @@
+const EventEmitter = require('events');
+const myEmitter = new EventEmitter();
+
+myEmitter.on('myEvent', (data) => {
+  console.log(`Произошло событие myEvent, сообщение: ${data.message}`);
+});
+
+myEmitter.prependListener('myEvent', () => {
+  console.log(`Последним добавлен — первым вызван`);
+});
+
+myEmitter.emit('myEvent', {message: "Привет, Мир!"});
+
